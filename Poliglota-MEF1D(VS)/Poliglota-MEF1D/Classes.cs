b994f2f3c9bffccc4bc65593e409e8685d7f8b16
SalvadOlivo <00264217@uca.edu.sv>
2021-05-11 17:38:35 -0600

    proyecto de VS

---
+++ Poliglota-MEF1D(VS)/Poliglota-MEF1D/Classes.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Math;
+using static Poliglota_MEF1D.Tools;
+using static Poliglota_MEF1D.MathTools;
+using static Poliglota_MEF1D.Sel;
+
+namespace Poliglota_MEF1D
+{
+    class Classes
+    {
+
+		public enum lines
+		{
+			NOLINE,
+			SINGLELINE,
+			DOUBLELINE
+		}
+		public enum modes
+		{
+			NOMODE,
+			INT_FLOAT,
+			INT_INT_INT
+		}
+		public enum parameters
+		{
+			ELEMENT_LENGTH,
+			THERMAL_CONDUCTIVITY,
+			HEAT_SOURCE
+		}
+		public enum sizes
+		{
+			NODES,
+			ELEMENTS,
+			DIRICHLET,
+			NEUMANN
+		}
+
+		//Clase abstracta que representa un objeto en la malla
+		public abstract class item
+		{
+			protected int id; //identificador
+			protected float x; //coordenada en X (basta con este dato por estar en 1 dimensiÃ³n)
+			protected int node1; //identificador de nodo
+			protected int node2; //segundo identificador de nodo
+			protected float value; //valor asociado al objeto
+								   //Getters para los atributos
+			public int getId()
+			{
+				return id;
+			}
+
+			public float getX()
+			{
+				return x;
+			}
+
+			public int getNode1()
+			{
+				return node1;
+			}
+
+			public int getNode2()
+			{
+				return node2;
+			}
+
+			public float getValue()
+			{
+				return value;
+			}
+
+			//MÃ©todos abstractos para instanciar los atributos de acuerdo a las necesidades
+
+			//Caso en que se utiliza un entero y un real
+			public abstract void setIntFloat(int n, float r);
+
+			//Caso en que se utilizan tres enteros
+			public abstract void setIntIntInt(int n1, int n2, int n3);
+
+		}
+
+		//Clase que representa cada nodo de la malla
+		public class node : item
+		{
+
+			//Un nodo usa un entero y un real: su identificador, y su coordenada en X
+			public override void setIntFloat(int identifier, float x_coordinate)
+			{
+				id = identifier;
+				x = x_coordinate;
+			}
+
+			public override void setIntIntInt(int n1, int n2, int n3)
+			{
+			}
+
+		}
+
+		//Clase que representa un elemento en la malla
+		public class element : item
+		{
+
+			public override void setIntFloat(int n1, float r)
+			{
+			}
+
+			//Un elemento usa tres enteros: su identificador, y los identificadores de sus nodos
+			public override void setIntIntInt(int identifier, int firstnode, int secondnode)
+			{
+				id = identifier;
+				node1 = firstnode;
+				node2 = secondnode;
+			}
+
+		}
+
+		//Clase que representa una condiciÃ³n impuesta en un nodo de la malla
+		public class condition : item
+		{
+
+			//Una condiciÃ³n usa un entero y un real: un identificador de nodo, y un valor a aplicar
+			public override void setIntFloat(int node_to_apply, float prescribed_value)
+			{
+				node1 = node_to_apply;
+				value = prescribed_value;
+			}
+
+			public override void setIntIntInt(int n1, int n2, int n3)
+			{
+			}
+
+		}
+
+		//Clase que representa la malla del problema
+		public class mesh
+		{
+			private float[] parameters = new float[3]; //Para este caso, los valores de l, k y Q
+			private int[] sizes = new int[4]; //La cantidad de nodos, elementos, condiciones de dirichlet y neumann
+			private node[] node_list; //Arreglo de nodos
+			private element[] element_list; //Arreglo de elementos
+			private condition[] dirichlet_list; //Arreglo de condiciones de Dirichlet
+			private condition[] neumann_list; //Arreglo de condiciones de Neumann
+											  //MÃ©todo para instanciar el arreglo de parÃ¡metros, almacenando los
+											  //valores de l, k y Q, en ese orden
+			public void setParameters(float l, float k, float Q)
+			{
+				parameters[(int)parameters.ELEMENTS_LENGTH] = 1;
+				parameters[(int)parameters.THERMAL_CONDUCTIVITY] = k;
+				parameters[(int)parameters.HEAT_SOURCE] = Q;
+			}
+
+			//MÃ©todo para instanciar el arreglo de cantidades, almacenando la cantidad
+			//de nodos, de elementos, y de condiciones (de Dirichlet y de Neumann)
+			public void setSizes(int nnodes, int neltos, int ndirich, int nneu)
+			{
+				sizes[(int)sizes.NODES] = nnodes;
+				sizes[(int)sizes.ELEMENTS] = neltos;
+				sizes[(int)sizes.DIRICHLET] = ndirich;
+				sizes[(int)sizes.NEUMANN] = nneu;
+			}
+
+			//MÃ©todo para obtener una cantidad en particular
+			public int getSize(int s)
+			{
+				return sizes[s];
+			}
+
+			//MÃ©todo para obtener un parÃ¡metro en particular
+			public float getParameter(int p)
+			{
+				return parameters[p];
+			}
+
+			//MÃ©todo para instanciar los cuatro atributos arreglo, usando
+			//las cantidades definidas
+			public void createData()
+			{
+				node_list = Arrays.InitializeWithDefaultInstances<node>(sizes[(int)sizes.NODES]);
+				element_list = Arrays.InitializeWithDefaultInstances<element>(sizes[(int)sizes.ELEMENTS]);
+				dirichlet_list = Arrays.InitializeWithDefaultInstances<condition>(sizes[(int)sizes.DIRICHLET]);
+				neumann_list = Arrays.InitializeWithDefaultInstances<condition>(sizes[(int)sizes.NEUMANN]);
+			}
+
+			//Getters para los atributos arreglo
+			public node getNodes()
+			{
+				return node_list;
+			}
+			public element getElements()
+			{
+				return element_list;
+			}
+			public condition getDirichlet()
+			{
+				return dirichlet_list;
+			}
+			public condition getNeumann()
+			{
+				return neumann_list;
+			}
+
+			//MÃ©todo para obtener un nodo en particular
+			public node getNode(int i)
+			{
+				// Se determinó que la siguiente línea contiene una llamada al constructor de copia; esto debe verificarse
+				//y debe crearse un constructor de copia
+				//ORIGINAL: return node_list[i];
+				return new node(node_list[i]);
+			}
+
+			//MÃ©todo para obtener un elemento en particular
+			public element getElement(int i)
+			{
+				// Se determinó que la siguiente línea contiene una llamada al constructor de copia; esto debe verificarse
+				//y debe crearse un constructor de copia
+				//ORIGINAL: return element_list[i];
+				return new element(element_list[i]);
+			}
+
+			//MÃ©todo para obtener una condiciÃ³n en particular
+			//(ya sea de Dirichlet o de Neumann)
+			public condition getCondition(int i, int type)
+			{
+				if (type == (int)sizes.DIRICHLET)
+				{
+					// Se determinó que la siguiente línea contiene una llamada al constructor de copia; esto debe verificarse
+					//y debe crearse un constructor de copia
+					//ORIGINAL: return dirichlet_list[i];
+					return new condition(dirichlet_list[i]);
+				}
+				else
+				{
+					// Se determinó que la siguiente línea contiene una llamada al constructor de copia; esto debe verificarse
+					//y debe crearse un constructor de copia
+					//ORIGINA: return neumann_list[i];
+					return new condition(neumann_list[i]);
+				}
+			}
+		}
+
+
+		internal static class Arrays
+		{
+			public static T[] InitializeWithDefaultInstances<T>(int length) where T : new()
+			{
+				T[] array = new T[length];
+				for (int i = 0; i < length; i++)
+				{
+					array[i] = new T();
+				}
+				return array;
+			}
+
+			public static void DeleteArray<T>(T[] array) where T : System.IDisposable
+			{
+				foreach (T element in array)
+				{
+					if (element != null)
+						element.Dispose();
+				}
+			}
+		}
+
+
+
+	}
+}
---
+++ Poliglota-MEF1D(VS)/Poliglota-MEF1D/main2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Poliglota_MEF1D.Classes;
+using static Poliglota_MEF1D.Tools;
+using static Poliglota_MEF1D.Sel;
+using static Poliglota_MEF1D.MathTools;
+using System.Numerics;
+
+namespace Poliglota_MEF1D
+{
+    class Main2
+    {
+        public static int Main()
+        {
+			List<Matrix> localKs = new List<Matrix>();
+			List<Vector> localbs = new List<Vector>();
+
+			//Se preparan tambiÃ©n las variables para la K y la b globales, y una para las incÃ³gnitas de
+			//temperatura, que es donde se almacenarÃ¡ la respuesta.
+			Matrix K = new Matrix();
+			Vector b = new Vector();
+			Vector T = new Vector();
+
+			//Se coloca primero una introducciÃ³n con todas las caracterÃ­sticas de la implementaciÃ³n
+			Console.Write("IMPLEMENTACIÃ“N DEL MÃ‰TODO DE LOS ELEMENTOS FINITOS\n");
+			Console.Write("\t- TRANSFERENCIA DE CALOR\n");
+			Console.Write("\t- 1 DIMENSIÃ“N\n");
+			Console.Write("\t- FUNCIONES DE FORMA LINEALES\n");
+			Console.Write("\t- PESOS DE GALERKIN\n");
+			Console.Write("*********************************************************************************\n\n");
+
+			//Se crea un objeto mesh, que contendrÃ¡ toda la informaciÃ³n de la malla
+			mesh m = new mesh();
+			//Se procede a obtener toda la informaciÃ³n de la malla y almacenarla en m
+			leerMallayCondiciones(m);
+
+			//Se procede a crear la K local y la b local de cada elemento, almacenando estas
+			//estructuras en los vectores localKs y localbs
+			crearSistemasLocales(m, localKs, localbs);
+			//Descomentar la siguiente lÃ­nea para observar las Ks y bs creadas
+			//showKs(localKs); showbs(localbs);
+
+			//Se inicializan con ceros la K global y la b global
+			zeroes(K, m.getSize(NODES));
+			zeroes(b, m.getSize(NODES));
+			//Se procede al proceso de ensamblaje
+			ensamblaje(m, localKs, localbs, K, b);
+			//Descomentar la siguiente lÃ­nea para observar las estructuras ensambladas
+			//showMatrix(K); showVector(b);
+
+			//Se aplican primero las condiciones de contorno de Neumann
+			applyNeumann(m, b);
+			//Descomentar la siguiente lÃ­nea para observar los cambios en b
+			//showVector(b);
+
+			//Luego se aplican las condiciones de contorno de Dirichlet
+			applyDirichlet(m, K, b);
+			//Descomentar la siguiente lÃ­nea para observar el SEL final luego
+			//de los cambios provocados por Dirichlet
+			//showMatrix(K); showVector(b);
+
+			//Se prepara con ceros el vector T que contendrÃ¡ las respuestas
+			zeroes(T, b.size());
+			//Finalmente, se procede a resolver el SEL
+			calculate(K, b, T);
+
+			//Se informa la respuesta:
+			Console.Write("La respuesta es: \n");
+			showVector(T);
+
+			return 0;
+		}
+
+
+	}
+
+}
